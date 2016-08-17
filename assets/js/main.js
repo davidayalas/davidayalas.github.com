6a01cba65e93ec60a838e4e870ab97aeaa560053
cs-canigo <david.ayala@LT2B034004.ctti.local>
2016-08-17 13:46:55 +0200

    new look

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,15 +66,15 @@
 		// Off-Canvas Navigation.
 
 			// Navigation Button.
-				$(
+				/*$(
 					'<div id="navButton">' +
 						'<a href="#navPanel" class="toggle"></a>' +
 					'</div>'
 				)
-					.appendTo($body);
+					.appendTo($body);*/
 
 			// Navigation Panel.
-				$(
+				/*$(
 					'<div id="navPanel">' +
 						'<nav>' +
 							$('#nav').navList() +
@@ -90,7 +90,7 @@
 						resetForms: true,
 						target: $body,
 						visibleClass: 'navPanel-visible'
-					});
+					});*/
 
 			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
 				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
